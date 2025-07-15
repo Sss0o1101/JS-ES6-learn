1f8ec02cac30cc6a9538f14e3629a0d7bd656207
alias <matusoyo2436@gmail.com>
2025-07-15 20:30:39 +0900

    new tsxt

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,1 +1,23 @@
+//alert('hello world');
 
+//console.log('test');
+
+const foo = 1 + 1;
+console.log(foo);
+
+const bar = 2 * 2;
+console.log(bar);
+
+const baz = 3 / 3;
+console.log(baz);
+
+const qux = 4 - 4;
+console.log(qux);
+
+const quux = 5 % 5;
+console.log(quux);
+
+const quuux = 6 ** 6;
+console.log(quuux);
+
+// foo, bar, test, x, foobar, hoge, fuga, piyo
