af658afc2d5264b5875406271cfaad100cdb5d7b
alias <matusoyo2436@gmail.com>
2025-07-25 21:55:00 +0900

    no message

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -6,23 +6,23 @@
 
 //console.log('test');
 
-const foo = 1 + 1;
-console.log(foo);
+    const foo = 1 + 1;
+    console.log(foo);
 
-const bar = 2 * 2;
-console.log(bar);
+    const bar = 2 * 2;
+    console.log(bar);
 
-const baz = 3 / 3;
-console.log(baz);
+    const baz = 3 / 3;
+    console.log(baz);
 
-const qux = 4 - 4;
-console.log(qux);
+    const qux = 4 - 4;
+    console.log(qux);
 
-const quux = 5 % 5;
-console.log(quux);
+    const quux = 5 % 5;
+    console.log(quux);
 
-const quuux = 6 ** 6;
-console.log(quuux);
+    const quuux = 6 ** 6;
+    console.log(quuux);
 
 // foo, bar, test, x, foobar, hoge, fuga, piyo
 
@@ -44,12 +44,12 @@ console.log(quuux);
 変数  ------------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
-const x = 10;
-console.log(x); // 10
+    const x = 10;
+    console.log(x); // 10
 
-let y = 20;
-y = 30;
-console.log(y); // 30
+    let y = 20;
+    y = 30;
+    console.log(y); // 30
 
 
 
@@ -57,37 +57,37 @@ console.log(y); // 30
 データ型 ------------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
-console.log(typeof x);             // number
-console.log(typeof 'Hello World'); // string
-console.log(typeof 10);            // number
-console.log(typeof true);          // boolean
-console.log(typeof null);          // object
-console.log(typeof undefined);     // undefined
-console.log(typeof NaN);           // number
-console.log(typeof Infinity);      // number
-console.log(typeof -Infinity);     // number
-console.log(typeof 0);             // number
-console.log(typeof 1);             // number
-console.log(typeof 2);             // number
+    console.log(typeof x);             // number
+    console.log(typeof 'Hello World'); // string
+    console.log(typeof 10);            // number
+    console.log(typeof true);          // boolean
+    console.log(typeof null);          // object
+    console.log(typeof undefined);     // undefined
+    console.log(typeof NaN);           // number
+    console.log(typeof Infinity);      // number
+    console.log(typeof -Infinity);     // number
+    console.log(typeof 0);             // number
+    console.log(typeof 1);             // number
+    console.log(typeof 2);             // number
 
 
 /*
 配列 ------------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(array);  // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        console.log(array);  // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-console.log(array[0]); // 1
-console.log(array[1]); // 2
-console.log(array[2]); // 3
-console.log(array[3]); // 4
-console.log(array[4]); // 5
-console.log(array[5]); // 6
-console.log(array[6]); // 7
-console.log(array[7]); // 8
-console.log(array[8]); // 9
-console.log(array[9]); // 10
+        console.log(array[0]); // 1
+        console.log(array[1]); // 2
+        console.log(array[2]); // 3
+        console.log(array[3]); // 4
+        console.log(array[4]); // 5
+        console.log(array[5]); // 6
+        console.log(array[6]); // 7
+        console.log(array[7]); // 8
+        console.log(array[8]); // 9
+        console.log(array[9]); // 10
 
 
 
@@ -95,27 +95,27 @@ console.log(array[9]); // 10
 ループ文 ------------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
 
-}
+    }
 
-// --------------------------------------------------------------
+    // --------------------------------------------------------------
 
-const questions = [
-    '現在の日本の総理利大臣の名前は？',
-    '令和7年は西暦で言うと何年？',
-    '最も人口が多い国はどこ？'
-];
+    const questions = [
+        '現在の日本の総理利大臣の名前は？',
+        '令和7年は西暦で言うと何年？',
+        '最も人口が多い国はどこ？'
+    ];
 
-for (let index = 0; index < questions.length; index++) {
-    console.log(questions[index]);
-}
+    for (let index = 0; index < questions.length; index++) {
+        console.log(questions[index]);  // 現在の日本の総理利大臣の名前は？ 令和7年は西暦で言うと何年？ 最も人口が多い国はどこ？
+    }
 
-// 同じ
-console.log(questions[0]);
-console.log(questions[1]);
-console.log(questions[2]);
+    // 同じ
+    console.log(questions[0]);  // 現在の日本の総理利大臣の名前は？
+    console.log(questions[1]);  // 令和7年は西暦で言うと何年？
+    console.log(questions[2]);  // 最も人口が多い国はどこ？
 
 
 // forEach -------------------------------------------------------
@@ -127,7 +127,7 @@ const questions2 = [
 ];
 
 questions2.forEach((question) => {
-    console.log(question);
+    console.log(question); // 現在のアメリカの大統領の名前は？ 令和8年は西暦で言うと何年？ 最も人口が多い都市はどこ？
 });
 
 
@@ -183,18 +183,3 @@ if (myMoney >= 500) {
 /*
 関数 ------------------------------------------------------------------------------------------------------------------------------------------------------
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
