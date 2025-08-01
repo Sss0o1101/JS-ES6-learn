7264ae853a544b031bfb63c932ab2677117014ad
alias <matusoyo2436@gmail.com>
2025-08-01 23:56:16 +0900

    no message

---
+++ lesson-2/script.js
@@ -0,0 +1,9 @@
+const snsUser2 = {
+  id: 1,
+  userName: 'Taro',
+  post: function (contents) {
+    return contents + 'を投稿しました by ...' + this.userName;
+  }
+}
+
+console.log(snsUser2.post('プログラミングなう。'));
