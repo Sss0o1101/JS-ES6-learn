b9785fd7e929cd62a830f4b5b10bdb19c09f4832
alias <matusoyo2436@gmail.com>
2025-12-11 23:32:23 +0900

    delete

--- lesson-10/script.js
+++ lesson-10/script.js
@@ -23,13 +23,3 @@
 /* ----------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
-/* .classList ------------------------------------------------------------------------------------------*/
-
-    const button = document.querySelector('button');
-    const p = document.querySelector('p');
-
-    button.addEventListener('click', () => {
-        p.classList.add('pink-bg', 'red-border');
-        p.classList.remove('green-color');
-    });
-
