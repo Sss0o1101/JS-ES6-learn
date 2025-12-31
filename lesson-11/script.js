a0b2c500ff357b62b5fb7062177eef1863b53093
alias <matusoyo2436@gmail.com>
2025-12-31 19:34:02 +0900

    日時をロケールに合わせて日付のみ表示

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -62,3 +62,4 @@ Date.now();  //現在の日時
 Date.parse();  //日時をミリ秒で返す
 Date.UTC();  //UTCの日時
 Date.prototype.toLocaleString();  //日時をロケールに合わせて表示
+Date.prototype.toLocaleDateString();  //日時をロケールに合わせて日付のみ表示
