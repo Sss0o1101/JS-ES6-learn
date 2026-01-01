0c0829ddd2db173ec9eecbce62a09665811725e3
alias <matusoyo2436@gmail.com>
2026-01-01 23:54:38 +0900

    要素の属性を取得

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -81,3 +81,4 @@ Element.prototype.appendChild();  //要素を追加
 Element.prototype.insertBefore();  //要素を挿入
 Element.prototype.replaceChild();  //要素を置換
 Element.prototype.cloneNode();  //要素を複製
+Element.prototype.getAttribute();  //要素の属性を取得
