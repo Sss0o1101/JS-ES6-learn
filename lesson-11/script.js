48a8513549f20dcbd968959c4322908a1d1db9b9
alias <matusoyo2436@gmail.com>
2026-01-01 23:53:09 +0900

    日時をロケールに合わせて時間のみ表示

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -73,3 +73,4 @@ Date.prototype.toFixed();  //日時を固定小数点形式で返す
 Date.prototype.toPrecision();  //日時を精度形式で返す
 Date.prototype.toLocaleString();  //日時をロケールに合わせて表示
 Date.prototype.toLocaleDateString();  //日時をロケールに合わせて日付のみ表示
+Date.prototype.toLocaleTimeString();  //日時をロケールに合わせて時間のみ表示
