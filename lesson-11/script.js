7334517e5905c7a45f1e9501e92f726d53c0e8c1
alias <matusoyo2436@gmail.com>
2026-01-01 23:54:15 +0900

    要素を置換

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -79,3 +79,4 @@ Date.prototype.toLocaleTimeString();  //日時をロケールに合わせて時�
 
 Element.prototype.appendChild();  //要素を追加
 Element.prototype.insertBefore();  //要素を挿入
+Element.prototype.replaceChild();  //要素を置換
