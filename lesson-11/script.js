e9fd162e5caafffe995e5c67616131aa85dfe4e5
alias <matusoyo2436@gmail.com>
2026-01-07 23:35:11 +0900

    コピーされたときに発火

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -103,7 +103,7 @@ EventTarget.prototype.oncanplay();  //再生可能になったときに発火
 EventTarget.prototype.oncanplaythrough();  //再生可能になったときに発火
 EventTarget.prototype.onchange();  //変更されたときに発火
 EventTarget.prototype.oncontextmenu();  //コンテキストメニューが表示されたときに発火
-// EventTarget.prototype.oncopy();  //コピーされたときに発火
+EventTarget.prototype.oncopy();  //コピーされたときに発火
 // EventTarget.prototype.oncut();  //切り取られたときに発火
 // EventTarget.prototype.onpaste();  //貼り付けられたときに発火
 // EventTarget.prototype.oninput();  //入力されたときに発火
