d860486363eebd3964dba4f32e6fff83da2c82f7
alias <matusoyo2436@gmail.com>
2026-02-08 23:34:39 +0900

    new Date

---
+++ lesson-12/script.js
@@ -0,0 +1,13 @@
+//time
+
+const time = new Date();
+console.log(time);
+
+// const time2 = new Date('2026-02-08 10:00:00');
+// console.log(time2);
+
+// const time3 = new Date('2026-02-08T10:00:00');
+// console.log(time3);
+
+// const time4 = new Date('2026-02-08T10:00:00+09:00');
+// console.log(time4);
