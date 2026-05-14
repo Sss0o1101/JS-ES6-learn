0b39b7d2853057f5f168bf4e671e21f26eb32e82
alias <matusoyo2436@gmail.com>
2026-05-14 23:10:07 +0900

    カウンター

---
+++ lesson-16/script.js
@@ -0,0 +1,11 @@
+// カウンター
+
+let count = 0;
+
+const btnElement = document.querySelector('button');
+const pElement = document.querySelector('p');
+
+btnElement.addEventListener('click', () => {
+  count++;
+  pElement.textContent = count;
+});
