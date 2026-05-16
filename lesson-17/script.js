b18002e43f6acf120f0f8a5a170181c4f5ce1431
alias <matusoyo2436@gmail.com>
2026-05-16 23:28:42 +0900

    定数

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -18,3 +18,12 @@
     document.querySelector('#colorText').textContent = `カラーコード : ${document.querySelector('#colorPicker').value}`;  // カラーコード : #000000
 
 //------------------------------------------------------------------------------
+
+const names = "sss";
+    console.log(names);
+
+    //-------------------
+    const text = document.querySelector("#colorText");
+    const color = document.querySelector("#colorPicker");
+
+    text.textContent =`カラーコード : ${color.value}`;   // カラーコード : #000000
