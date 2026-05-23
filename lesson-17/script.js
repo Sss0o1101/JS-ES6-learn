81ef7cd920b487cdd0736927420c0ed5334483a5
alias <matusoyo2436@gmail.com>
2026-05-23 23:34:42 +0900

    別の書き方(省略) !

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -189,3 +189,7 @@ isAgreed.addEventListener('change', () => {
 
 const isAgreed = document.querySelector('#check');
 const btn = document.querySelector('#btn');
+
+isAgreed.addEventListener('change', () => {
+        btn.disabled = !isAgreed.checked;
+});
