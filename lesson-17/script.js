44c2bdbe446929829dd7716e94f88a3259f03fef
alias <matusoyo2436@gmail.com>
2026-05-25 23:33:55 +0900

    ボタンをクリックしてダークモード

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -284,3 +284,13 @@ window.addEventListener('load', () => {
 
 
 //------------------------------------------------------------------------
+
+
+/* ボタンをクリックしてダークモード () -------------------------------------------------------------*/
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    //クリックされた時の処理
+    document.body.classList.toggle('dark-theme');
+});
