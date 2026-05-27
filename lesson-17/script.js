a9850a3e9df5b9374c613d2a2c8e217c3d16156d
alias <matusoyo2436@gmail.com>
2026-05-27 20:28:10 +0900

    no message

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -392,3 +392,5 @@ const getScrollPercent = () => {
         const scrolled = window.scrollY;
         console.log(`${scrolled}スクロールされました`);
 };
+
+window.addEventListener('scroll', getScrollPercent );
