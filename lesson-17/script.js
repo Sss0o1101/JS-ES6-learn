c69d89c1561112ac7238ffb41676657e82781a77
alias <matusoyo2436@gmail.com>
2026-05-27 20:23:40 +0900

    文字によって表示を変える()

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -330,3 +330,13 @@ text.addEventListener('keyup', () => {
 
 const text = document.querySelector('#text');
 const count = document.querySelector('#count');
+
+text.addEventListener('keyup', () => {
+        count.textContent = text.value.length;
+
+        if (text.value.length > 5 ) {
+            count.classList.add('alert');
+        } else {
+            count.classList.remove('alert');
+        }
+});
