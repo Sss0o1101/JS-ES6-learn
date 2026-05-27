b2102570422a1ab12442c2f43c52218ea2051d12
alias <matusoyo2436@gmail.com>
2026-05-27 20:20:59 +0900

    ボタンのテキストを変更

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -303,3 +303,13 @@ btn.addEventListener('click', () => {
 /* if,else | ボタンのテキストを変更 ------------------------------------------------------------------------------------------*/
 
 const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+
+        if (btn.textContent === "ダークモードにする") {
+            btn.textContent = "ライトモードにする";
+        } else {
+            btn.textContent = "ダークモードにする";
+        }
+});
