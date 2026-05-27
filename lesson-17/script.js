e8f7fa08f48b5e7107fc04bcddd8681671f23403
alias <matusoyo2436@gmail.com>
2026-05-27 20:25:10 +0900

    チェックでボタンを有効化

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -346,3 +346,12 @@ text.addEventListener('keyup', () => {
 
 const isAgreed = document.querySelector('#check');
 const btn = document.querySelector('#btn');
+
+isAgreed.addEventListener('change', () => {
+        //チェックボックスにチェックが入っているなら
+        if (isAgreed.checked === true) {
+            btn.disabled = false;  //ボタンの無効化を無しにする
+        } else {
+            btn.disabled = true;   //ボタンを無効化にする
+        }
+});
