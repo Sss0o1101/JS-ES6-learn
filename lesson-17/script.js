b543697b178f94f6486f17601c8c09aa08e6f422
alias <matusoyo2436@gmail.com>
2026-06-02 23:57:03 +0900

    文字によって表示を変える

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -508,3 +508,13 @@ const text = document.querySelector('#text');
 
 const text = document.querySelector('#text');
 const count = document.querySelector('#count');
+
+text.addEventListener('keyup', () => {
+        count.textContent = text.value.length;
+
+        if (text.value.length > 5 ) {
+            count.classList.add('alert');
+        } else {
+            count.classList.remove('alert');
+        }
+});
