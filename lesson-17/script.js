20bff50b477cceaf5250f14f9a4b6e938629eb31
alias <matusoyo2436@gmail.com>
2026-06-04 23:18:53 +0900

    オブジェクトで画像、メニュー名、値段をまとめ

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -650,3 +650,9 @@ const menu = document.querySelector('#menu');
         'fig.jpg',
         'apple.jpg',
     ];
+
+    for (let i = 0; i < lists.length; i++) {
+        const content = `<div><img src="./images/${lists[i]}" alt=""></div>`;
+        menu.insertAdjacentHTML('beforeend', content);
+    }
+
