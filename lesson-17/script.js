e47e9e26d51a6341d75b65a5dd6355b23a607253
alias <matusoyo2436@gmail.com>
2026-06-04 23:11:05 +0900

    no message

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -564,3 +564,12 @@ const menu = document.querySelector('#menu');
         'fig.jpg',
         'apple.jpg',
     ];
+
+    //６個の画像の表示
+    const content = `<div><img src="./images/${lists[0]}" alt=""></div>
+        <div><img src="./images/${lists[1]}" alt=""></div>
+        <div><img src="./images/${lists[2]}" alt=""></div>
+        <div><img src="./images/${lists[3]}" alt=""></div>
+        <div><img src="./images/${lists[4]}" alt=""></div>
+        <div><img src="./images/${lists[5]}" alt=""></div>
+    `;
