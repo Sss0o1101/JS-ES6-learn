46668af8f74cc1e9e400f3162e5893f0f47708bb
alias <matusoyo2436@gmail.com>
2026-06-05 23:48:28 +0900

    no message

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -694,3 +694,37 @@ const menu = document.querySelector('#menu');
 /* 配列とオブジェクトをひとまとめにしよう ---------------------------------------------------------------------------------------------------------------*/
 
     const menu = document.querySelector('#menu');
+
+    const lists = [
+        {
+            name: 'イチゴ',
+            img: 'strawberry.jpg',
+            price: 450,
+        },
+        {
+            name: 'ライム',
+            img: 'lime.jpg',
+            price: 400,
+        },
+        {
+            name: 'マンゴー',
+            img: 'mango.jpg',
+            price: 500,
+        },
+        {
+            name: 'レモン',
+            img: 'lemon.jpg',
+            price: 400,
+        },
+        {
+            name: 'イチジク',
+            img: 'fig.jpg',
+            price: 500,
+        },
+        {
+            name: 'リンゴ',
+            img: 'apple.jpg',
+            price: 400,
+        },
+    ];
+
