473f6d52054d50ea786fa9a91cdfc763deca82a4
alias <matusoyo2436@gmail.com>
2026-06-05 23:53:55 +0900

    no message

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -786,3 +786,36 @@ const menu = document.querySelector('#menu');
 /* 分割代入でコードをスッキリさせよう ---------------------------------------------------------------------------------------------------------------*/
 
     const menu = document.querySelector('#menu');
+
+    const lists = [
+        {
+            name: 'イチゴ',
+            img: 'strawberry.jpg',
+            price: 450,
+        },
+        {
+            name: 'ライム',
+            img: 'lime.jpg',
+            price: 400,
+        },
+        {
+            name: 'マンゴー',
+            img: 'mango.jpg',
+            price: 500,
+        },
+        {
+            name: 'レモン',
+            img: 'lemon.jpg',
+            price: 400,
+        },
+        {
+            name: 'イチジク',
+            img: 'fig.jpg',
+            price: 500,
+        },
+        {
+            name: 'リンゴ',
+            img: 'apple.jpg',
+            price: 400,
+        },
+    ];
