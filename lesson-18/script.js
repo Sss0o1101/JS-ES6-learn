f55dd34b536dcb666de7eb6f440b273156336337
alias <matusoyo2436@gmail.com>
2026-06-20 23:21:00 +0900

    no message

--- lesson-18/script.js
+++ lesson-18/script.js
@@ -4,3 +4,5 @@
 
     const loadingAreaGrey = document.querySelector('#loading');
     const loadingAreaGreen = document.querySelector('#loading-screen');
+    const loadingText = document.querySelector('#loading p');
+
