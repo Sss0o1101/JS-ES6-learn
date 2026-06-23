c34edd460b76fa4f2cb013c8837eeb3664e49059
alias <matusoyo2436@gmail.com>
2026-06-23 23:48:57 +0900

    // ローディング中テキスト

--- lesson-18/script.js
+++ lesson-18/script.js
@@ -33,3 +33,23 @@
       fill: 'forwards',
     }
   );
+
+  // ローディング中テキスト
+  loadingText.animate(
+    [
+      {
+        opacity: 1,
+        offset: .8  //80%
+      },
+      {
+        opacity: 0,
+        offset: 1  //100%
+      },
+    ],
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
