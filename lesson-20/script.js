3a332ec87918dc500c70f59e03d0bdd16d974bf1
alias <matusoyo2436@gmail.com>
2026-07-08 23:18:06 +0900

    コンソールでモンスターを倒す

--- lesson-20/script.js
+++ lesson-20/script.js
@@ -286,3 +286,10 @@
     let enemy2 = 100;
     let count2 = 0;
     window.alert('戦闘スタート');
+
+    while(enemy > 0) {
+        const attack = Math.floor(Math.random() * 30) +1;
+        console.log(`モンスターに${attack}のダメージ`);
+        enemy -= attack;
+        count += 1;
+    }
