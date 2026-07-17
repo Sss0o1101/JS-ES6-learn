64c8d9047bad7aa167526733137e217fedc8b391
alias <matusoyo2436@gmail.com>
2026-07-17 23:03:39 +0900

    no message

--- lesson-20/script.js
+++ lesson-20/script.js
@@ -466,14 +466,14 @@
     const number3 = Math.floor(Math.random() * 6);
     const answer10 = parseInt(window.prompt('数当てゲーム。0~5の数字を入力してね'));
 
-    let message;
-    if (answer === number) {
-        message = 'あたり！';
-    } else if (answer < number) {
-        message = '残念でした！もっと大きい';
-    } else if (answer > number) {
-        message = '残念でした！もっと小さい';
+    let message2;
+    if (answer10 === number3) {
+        message2 = 'あたり！';
+    } else if (answer10 < number3) {
+        message2 = '残念でした！もっと大きい';
+    } else if (answer10 > number3) {
+        message2 = '残念でした！もっと小さい';
     } else {
-        message = '0~5の数字を入力してね';
+        message2 = '0~5の数字を入力してね';
     }
-    window.alert(message);
+    window.alert(message2);
