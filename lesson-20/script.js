61df80cd68448fb8e1d97ef4a339d57296dbccf9
alias <matusoyo2436@gmail.com>
2026-07-17 23:02:54 +0900

    数当てゲーム

--- lesson-20/script.js
+++ lesson-20/script.js
@@ -467,3 +467,12 @@
     const answer10 = parseInt(window.prompt('数当てゲーム。0~5の数字を入力してね'));
 
     let message;
+    if (answer === number) {
+        message = 'あたり！';
+    } else if (answer < number) {
+        message = '残念でした！もっと大きい';
+    } else if (answer > number) {
+        message = '残念でした！もっと小さい';
+    } else {
+        message = '0~5の数字を入力してね';
+    }
