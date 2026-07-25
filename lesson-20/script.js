91b3cc5a5d904c396eca6602cafe902ff1699ff0
alias <matusoyo2436@gmail.com>
2026-07-25 23:56:22 +0900

    days

--- lesson-20/script.js
+++ lesson-20/script.js
@@ -658,4 +658,5 @@
         const sec = Math.floor(rest / 1000) % 60;
         const min = Math.floor(rest / 1000 / 60) % 60;
         const hours = Math.floor(rest / 1000 / 60 / 60) % 24;
+        const days = Math.floor(rest / 1000 / 60 / 60/ 24);
     }
