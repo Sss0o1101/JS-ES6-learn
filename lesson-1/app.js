0b8c15e5b8d79628c9dc4029ca8683a89fe93e41
alias <matusoyo2436@gmail.com>
2026-08-08 22:48:36 +0900

    no message

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -3,3 +3,8 @@ $post.setAttribute("class","post");
 $post.innerText = "お腹減ったなう。"
 
 const $timeline = document.querySelectorAll(".timeline")[0];
+$timeline.appendChild($post);
+
+
+// ---------------
+
