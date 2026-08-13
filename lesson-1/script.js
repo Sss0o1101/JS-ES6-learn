421f948f1f9055cedf6a19c4ec52bb792b68da46
alias <matusoyo2436@gmail.com>
2026-08-13 23:05:46 +0900

    window.history.length;

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -422,6 +422,7 @@ window.history.back(); // 前のページに戻る
 window.history.forward(); // 次のページに進む
 window.history.go(-1); // 前のページに戻る
 window.history.go(1); // 次のページに進む
+window.history.length; // 10
 
 document.querySelector("#p"); // <p>Hello World</p>
 document.querySelectorAll(".p"); // [<p>Hello World</p>, <p>Hello World</p>]
