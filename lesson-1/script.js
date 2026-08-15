9223e0d2aa7120b1e37a69510a22a5346364799c
alias <matusoyo2436@gmail.com>
2026-08-15 23:03:45 +0900

    no message

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -431,6 +431,7 @@ window.history.scrollBehavior; // smooth, auto, manual
 window.history.scrollTo(x, y); // スクロールを移動する
 window.history.scrollBy(x, y); // スクロールを移動する
 
+
 document.querySelector("#p"); // <p>Hello World</p>
 document.querySelectorAll(".p"); // [<p>Hello World</p>, <p>Hello World</p>]
 
