ccf315913a07e62d35134f070d3f87294f8d580a
alias <matusoyo2436@gmail.com>
2026-08-19 22:58:16 +0900

    swich

--- Fortune-telling App/script.js
+++ Fortune-telling App/script.js
@@ -4,5 +4,17 @@ const result = document.querySelector('#result');
 btn.addEventListener('click', () => {
     const n = Math.floor(Math.random() * 3);
 
-    result.textContent = n;
+    //result.textContent = n;
+
+    switch (n) {
+        case 0:
+            result.textContent = "大吉";
+            break;
+        case 1:
+            result.textContent = "中吉";
+            break;
+        case 2:
+            result.textContent = "凶";
+            break;
+    }
 });
