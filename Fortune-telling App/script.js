1633b6a6aed9c48ac9bb85d46a05dd33df65d42e
alias <matusoyo2436@gmail.com>
2026-08-19 23:03:08 +0900

    no message

--- Fortune-telling App/script.js
+++ Fortune-telling App/script.js
@@ -2,21 +2,11 @@ const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
 
 btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
-
     //result.textContent = n;
+    const result = ["大吉", "中吉", "凶"];
+    const n = Math.floor(Math.random() * 3);
+    result.textContent = result[n];
 
-    switch (n) {
-        case 0:
-            result.textContent = "大吉";
-            break;
-        case 1:
-            result.textContent = "中吉";
-            break;
-        case 2:
-            result.textContent = "凶";
-            break;
-    }
 });
 
 
@@ -26,6 +16,19 @@ btn.addEventListener('click', () => {
 //     const results = ["大吉", "中吉", "凶"];
 //     result.textContent = results[n];
 // });
+
+
+// switch (n) {
+//         case 0:
+//             result.textContent = "大吉";
+//             break;
+//         case 1:
+//             result.textContent = "中吉";
+//             break;
+//         case 2:
+//             result.textContent = "凶";
+//             break;
+//     }
 
 // if (n === 0) {
 //     result.textContent = "大吉";
