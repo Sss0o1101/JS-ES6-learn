5d9c4266b59083085c49adae8f8fd6bd357f34f3
alias <matusoyo2436@gmail.com>
2026-08-19 22:46:31 +0900

    .textContent = n

--- Fortune-telling App/script.js
+++ Fortune-telling App/script.js
@@ -2,6 +2,7 @@ const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
 
 btn.addEventListener('click', () => {
-    //result.textContent = "hit!"
     const n = Math.floor(Math.random() * 3);
+
+    result.textContent = n;
 });
