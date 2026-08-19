7d624be9a7c7f013e7eb9bb46b970a5c4455813e
alias <matusoyo2436@gmail.com>
2026-08-19 22:43:40 +0900

    no message

--- Fortune-telling App/script.js
+++ Fortune-telling App/script.js
@@ -2,5 +2,5 @@ const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
 
 btn.addEventListener('click', () => {
-    result.textContent = "hit!"
+    //result.textContent = "hit!"
 });
