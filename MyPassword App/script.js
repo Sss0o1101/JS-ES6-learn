62f59e53fec5badb0ce6a2581b26c919acb64a6a
alias <matusoyo2436@gmail.com>
2026-08-21 23:41:56 +0900

    result

--- MyPassword App/script.js
+++ MyPassword App/script.js
@@ -1,11 +1,12 @@
 const slider = document.querySelector('#slider');
 const passwordLength = document.querySelector('#password-length');
 const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
 
 slider.addEventListener('input', () => {
     passwordLength.textContent = slider.value;
 });
 
 btn.addEventListener('click', () => {
-
+    result.textContent = 'clicked!'
 });
