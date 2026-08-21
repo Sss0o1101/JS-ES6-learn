b943f19542a7f8470013d2d0935c91ae9feb6302
alias <matusoyo2436@gmail.com>
2026-08-21 23:28:50 +0900

    スライダーの値表示

--- MyPassword App/script.js
+++ MyPassword App/script.js
@@ -2,5 +2,5 @@ const slider = document.querySelector('#slider');
 const passwordLength = document.querySelector('#password-length');
 
 slider.addEventListener('input', () => {
-
-})
+    passwordLength.textContent = slider.value;
+});
