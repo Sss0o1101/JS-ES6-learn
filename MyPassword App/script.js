cf2b9b2da439a98efd7b9655c7957f2393199c98
alias <matusoyo2436@gmail.com>
2026-08-22 23:23:18 +0900

    8 文字のパスワード

--- MyPassword App/script.js
+++ MyPassword App/script.js
@@ -2,12 +2,22 @@ const slider = document.querySelector('#slider');
 const passwordLength = document.querySelector('#password-length');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
+
 const letters = "abcdefghijklmnopqrstuvwxyz";
+const password = "";
 
 slider.addEventListener('input', () => {
     passwordLength.textContent = slider.value;
 });
 
 btn.addEventListener('click', () => {
-    result.textContent = letters[Math.floor(Math.random() * 26)];
+    //result.textContent = letters[Math.floor(Math.random() * 26)];
+
+    for (let i = 0; i < 8; i++) {
+        password += letters[Math.floor(Math.random() * 26)];
+
+
+    }
+
+    result.textContent = password;
 });
