8b31836bc7a7c1ea852ca4b5c86a93aef797d63e
alias <matusoyo2436@gmail.com>
2026-08-22 23:13:42 +0900

    要素

--- MyPassword App/script.js
+++ MyPassword App/script.js
@@ -2,6 +2,7 @@ const slider = document.querySelector('#slider');
 const passwordLength = document.querySelector('#password-length');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
+const letters = "abcdefghijklmnopqrstuvwxyz";
 
 slider.addEventListener('input', () => {
     passwordLength.textContent = slider.value;
