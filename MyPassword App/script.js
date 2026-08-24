c0b449618a3ebb7b1a11ffd8c093d78492fb669d
alias <matusoyo2436@gmail.com>
2026-08-24 23:10:44 +0900

    Transition

--- MyPassword App/script.js
+++ MyPassword App/script.js
@@ -1,11 +1,12 @@
 const showPassword = () => {
+    const result = document.querySelector('#result');
 
 }
 
 const slider = document.querySelector('#slider');
 const passwordLength = document.querySelector('#password-length');
 const btn = document.querySelector('#btn');
-const result = document.querySelector('#result');
+//const result = document.querySelector('#result');
 const numbersCheckbox = document.querySelector('#numbers-checkbox');
 const symbolsCheckbox = document.querySelector('#symbols-checkbox');
 
