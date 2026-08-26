383de8500d6b23762dd972a48d2445ab94a220b4
alias <matusoyo2436@gmail.com>
2026-08-26 23:42:10 +0900

    no message

--- MyCountdownApp/script.js
+++ MyCountdownApp/script.js
@@ -5,6 +5,7 @@ const btn = document.querySelector('#btn');
 // (2) 残り時間を求める
 // (3) タイマーの終了
 
+// (1) 終了時刻を求める
 btn.addEventListener('click', () => {
     const endTime = new Date().getTime() + 3 * 1000;
 });
